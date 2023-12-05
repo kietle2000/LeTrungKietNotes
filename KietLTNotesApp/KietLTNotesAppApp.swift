18a51f5959905426d763daa613181200ca50b821
Lê Kiệt <kietlt@vihatgroup.com>
2023-12-05 09:42:12 +0700

    Initial Commit

---
+++ KietLTNotesApp/KietLTNotesAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  KietLTNotesAppApp.swift
+//  KietLTNotesApp
+//
+//  Created by Lê Kiệt on 12/5/23.
+//
+
+import SwiftUI
+
+@main
+struct KietLTNotesAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
